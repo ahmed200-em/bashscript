18829d81fc3cc80f512b6e05f565bbc5fb687ee4
ahmed200-em <almakrini2003@gmail.com>
2026-04-17 13:09:34 +0100

    add option ADD STUDENTS and SHOW STUDENTS

---
+++ manager.sh
@@ -0,0 +1,31 @@
+while true; 
+do
+echo "------ MENU ------"
+echo "1. Add students"
+echo "2. Show students"
+echo "3. Search students"
+echo "4. Exit"
+echo "Please choose from menu:"
+read menu
+if [ "$menu" -eq 1 ]; then
+	echo "---- ADD STUDENTS ---"
+	echo "What's your name?"
+	read name
+	echo "what's your age?"
+	read age
+		if [ $age -lt 18 ]; then
+			echo "Student is minor (not saved)"
+		else 
+		{
+			echo "---- STUDENTS LIST ----"
+			echo "Name: $name"
+			echo "Age: $age"
+			echo "-----------------"
+		} >> students.txt
+			echo "Student Saved"
+		fi
+elif [ "$menu" -eq 2 ]; then
+	echo "---- SHOW STUDENTS ----"
+	cat students.txt
+fi
+done
